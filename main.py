1cdd56246e2f8d7467127a663db119f03c650a02
AR_JPEG <58893337+AR-jpeg@users.noreply.github.com>
2020-02-08 20:13:53 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+os.system('pip install pyautogui')
+
+import pyautogui as gui
+import random
+
+for i in range(4354535345345435345435345345435):
+  gui.moveTo(random.randint(gui.screen()[0]),random.randint(gui.screen()[1]), duration = 0.9)
+ 
