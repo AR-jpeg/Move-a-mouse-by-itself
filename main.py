8f6890ee8732ee88905cf281868e3098768897d8
AR_JPEG <58893337+AR-jpeg@users.noreply.github.com>
2020-02-08 20:43:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ import pyautogui as gui
 import random
 
 for i in range(4354535345345435345435345345435):
-  gui.moveTo(random.randint(gui.screen()[0]),random.randint(gui.screen()[1]), duration = 0.9)
- 
+  gui.moveTo(random.randint(1,gui.size()[0]),random.randint(1,gui.size()[1]), duration = 0.9)
+#https://github.com/AR-jpeg/Move-a-mouse-by-itself/edit/master/LICENCE
